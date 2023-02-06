51a1451dc67812baa536e455c474884aa734dbfc
fanpeihua <80927307@yonghui.cn>
2023-02-06 10:16:26 +0800

    fix: final错用

--- android/src/main/java/io/flutter/plugins/webviewflutter/newActivity.java
+++ android/src/main/java/io/flutter/plugins/webviewflutter/newActivity.java
@@ -275,7 +275,7 @@ public class newActivity extends Activity {
                 break;
             case 2:
                 if (data != null) {
-                    final Uri[] uri = {data.getData()};
+                     Uri[] uri = {data.getData()};
 
                     if (uri[0] == null) {
                         Log.i("TAG", String.valueOf(data));
@@ -283,7 +283,7 @@ public class newActivity extends Activity {
                         String path = getCacheDir().getAbsolutePath();
 
                         try {
-                            final FileInputStream[] is = {null};
+                            FileInputStream[] is = {null};
                             try {
                                 Luban.with(this)
                                         .load(mFilePath)
